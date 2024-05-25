baa4fdbae5eca5e03fdadb306334335341c21874
hhk63 <hhk2@pdx.edu>
2024-05-25 00:10:47 -0700

    updated chatpage and loginpage

--- frontend/src/ChatPage/ChatPage.jsx
+++ frontend/src/ChatPage/ChatPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Box from '@mui/material/Box';
+import { useState } from "react";
 import './ChatPage.css'
 import { TextField, Button } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
@@ -7,8 +8,23 @@ import AddIcon from '@mui/icons-material/Add';
 import Bee from './Bee.jpg'
 import InputAdornment from '@mui/material/InputAdornment';
 import AddContact from "./AddContact/AddContact";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { map, concat } from 'lodash';
 
 export default function ChatPage () {
+    const [searchKey, setSearchKey] = useState("");
+    const [showContact, setShowContact] = useState(false);
+    const [newChat, setNewChat] = useState("");
+    const [allChats, setAllChats] = useState([]);
+    
+    /*function findUser() {
+        fetch(`http://localhost:5002/api/user?search=${searchKey}`, {
+            method: "GET",
+            headers: {
+                "Content-type" : "application/json"
+            }
+        }).then((response) => console.log(response))
+    }*/
     return (
         <div className='chat-page'>
             <Box
@@ -26,23 +42,46 @@ export default function ChatPage () {
                     backgroundColor: 'white' 
                 }}
             >
+                {/*<Button className='search-button' startIcon={<SearchIcon/>} onClick={() => findUser()}></Button>*/}
                 <TextField 
                     className='search-bar'  
                     variant='outlined' 
                     size='small' 
-                    InputProps={{
-                        startAdornment: (
-                          <InputAdornment position="start">
-                            <SearchIcon />
-                          </InputAdornment>
-                        ),
-                      }}
+                    onChange={(e) =>  setSearchKey(e.target.value)}
                 />
+                <Button 
+                    className='show-contact' 
+                    startIcon={<AccountCircleIcon sx={{color:"black"}}/>}
+                    onClick={() => setShowContact(true)}>
+                    Timmy T    
+                    </Button>
                 {/*<Button className='add-chat-button' variant='outlined' startIcon={<AddIcon/>}></Button>*/}
-                <AddContact></AddContact>
-
+                {/*<AddContact></AddContact>*/}
             </Box>
-            <Box
+            {!showContact && (
+                <Box
+                    className='chat-box'
+                    height={650}
+                    width={700}
+                    my={36}
+                    display="flex"
+                    alignItems="center"
+                    gap={4}
+                    p={2}
+                    sx={{ 
+                        border: '2px solid black',
+                        backgroundColor: 'white' 
+                    }}
+                >
+                    <h1 className='header'>Find Friends to Begin Chatting!</h1>
+                    <div className='bee'>
+                        <img src={Bee} alt="Bee" />
+                    </div>
+                
+                </Box>
+            )}
+            {showContact && (
+                <Box
                 className='chat-box'
                 height={650}
                 width={700}
@@ -56,12 +95,20 @@ export default function ChatPage () {
                     backgroundColor: 'white' 
                 }}
             >
-                <h1 className='header'>Find Friends to Begin Chatting!</h1>
-                <div className='bee'>
-                    <img src={Bee} alt="Bee" />
+                <h1 className='header'>Timmy T's Chats</h1>
+                <div className='new-chat'>
+                    {map(allChats, (chat) => {
+                        return (
+                            <div className='chat'>
+                                {chat}
+                            </div>
+                        )
+                    })}
                 </div>
-                
+                <TextField  className='send-chat' label='Enter Chat' variant='outlined' onChange={(e) =>  setNewChat(e.target.value)}/>
+                <Button className='new-chat' variant='outlined' label='add' onClick={() => setAllChats(concat(allChats, [newChat]))}></Button>
             </Box>
+        )}
         </div>
     )
 }
--- frontend/src/LoginPage/LoginForm.jsx
+++ frontend/src/LoginPage/LoginForm.jsx
@@ -1,9 +1,56 @@
 import { Box, Button, TextField } from "@mui/material";
 import React, { useState } from "react";
 import './LoginForm.css'
+import { useNavigate } from "react-router-dom";
+import Alert from '@mui/material/Alert';
 
 const LoginForm = ({}) => {
     const [showNewUserContent, setShowNewUserContent] = useState(false);
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate()
+    const [alert, setAlert] = useState(false);
+    
+    function authenticateInfo() {
+        fetch("http://localhost:5002/api/user/login", {
+            method: "POST",
+            body: JSON.stringify({
+                username, 
+                password
+            }),
+            headers: {
+                "Content-type" : "application/json"
+            }
+        }).then((response) => {
+            if(response.status == 200) {
+                navigate("/ChatPage")
+            }
+            if(response.status != 200) {
+                setAlert(true);
+            }
+        })
+    }
+    function createUser() {
+        fetch("http://localhost:5002/api/user/register", {
+            method: "POST",
+            body: JSON.stringify({
+                name,
+                username,
+                password
+            }),
+            headers: {
+                "Content-type" : "application/json"
+            }
+        }).then((response) => {
+            if(response.status == 200) {
+                navigate("/ChatPage")
+            }
+            if(response.status != 200) {
+                setAlert(true);
+            }
+        })
+    }
     return (
         <div className='login-form'>
             <Box
@@ -24,19 +71,21 @@ const LoginForm = ({}) => {
                 {!showNewUserContent && (
                     <div className='existing-user-content'>
                         <h1 className='header'>Buzz</h1>
-                        <TextField  className='username-field' label='Username' variant='outlined'/>
-                        <TextField  className='password-field' label='Password' variant='outlined'/>
-                        <Button className='submit-button'>Submit</Button>
+                        <TextField  className='username-field' label='Username' variant='outlined' onChange={(e) =>  setUsername(e.target.value)}/>
+                        <TextField  className='password-field' label='Password' type="password" variant='outlined' onChange={(e) =>  setPassword(e.target.value)}/>
+                        <Button className='submit-button' onClick={() => authenticateInfo()}>Submit</Button>
+                        {alert ? <Alert variant="filled" severity="error">Invalid Credentials</Alert> : <></> }
                         <Button className='not-a-user' variant="primary" onClick={() => setShowNewUserContent(true)}>Not a user? Make an account</Button>
                     </div>
                 )}
                 {showNewUserContent && (
                     <div className='new-user-content'>
                         <h1 className='new-header'>Registration</h1>
-                        <TextField  className='name-field' label='Full Name' variant='outlined'/>
-                        <TextField  className='username-field' label='Username' variant='outlined'/>
-                        <TextField  className='password-field' label='Password' variant='outlined'/>
-                        <Button className='new-submit-button'>Submit</Button>
+                        <TextField  className='name-field' label='Full Name' variant='outlined' onChange={(e) =>  setName(e.target.value)}/>
+                        <TextField  className='username-field' label='Username' variant='outlined' onChange={(e) =>  setUsername(e.target.value)}/>
+                        <TextField  className='password-field' label='Password' type="password" variant='outlined' onChange={(e) =>  setPassword(e.target.value)}/>
+                        <Button className='new-submit-button' variant="primary" onClick={() => createUser()}>Submit</Button>
+                        {alert ? <Alert variant="filled" severity="error">User Already Exists!</Alert> : <></> }
                     </div>
                 )}
             </Box>
