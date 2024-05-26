8528dc0445d5bef45b28acb010a5bd94bf1330fc
JT Strabala <166744338+strabalaj@users.noreply.github.com>
2024-05-25 21:27:24 -0700

    Delete backend directory
    out with the old

--- backend/server/config/generate_jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-const generate_jwt = (id) => {
-    return jwt.sign({id},process.env.JWT_SPECIAL_VALUE, {
-        expiresIn: "100d"
-    });
-};
-
-module.exports = generate_jwt;
